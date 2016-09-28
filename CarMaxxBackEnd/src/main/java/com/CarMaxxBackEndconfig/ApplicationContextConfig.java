340fc28a2191e4f3fc974a871381632e3f203a3f
imtiazahamedm <imtiazcrazy@gmail.com>
2016-09-28 15:48:40 +0530

    Add files via upload

---
+++ CarMaxxBackEnd/src/main/java/com/CarMaxxBackEndconfig/ApplicationContextConfig.java
@@ -0,0 +1,69 @@
+package com.CarMaxxBackEndconfig;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+import org.h2.engine.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan("com.CarMaxxBackEndconfig")
+@EnableTransactionManagement
+
+
+public class ApplicationContextConfig {
+ 
+	@Bean(name = "dataSource")
+	public DataSource getDataSource() {
+	    BasicDataSource dataSource = new BasicDataSource();
+	    dataSource.setDriverClassName("org.h2.Driver");
+	    dataSource.setUrl("jdbc:h2:tcp://localhost/~/test");
+	    dataSource.setUsername("sa");
+	    dataSource.setPassword("sa");
+	 
+	    return dataSource;
+	}
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+	 
+	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+	 
+	    sessionBuilder.addAnnotatedClasses(User.class);
+	 
+	    return sessionBuilder.buildSessionFactory();
+	    
+	}
+	private Properties getHibernateProperties() {
+	    Properties properties = new Properties();
+	    properties.put("hibernate.show_sql", "true");
+	    properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+	    return properties;
+	}
+	@Autowired
+	@Bean(name = "transactionManager")
+	public HibernateTransactionManager getTransactionManager(
+	        SessionFactory sessionFactory) {
+	    HibernateTransactionManager transactionManager = new HibernateTransactionManager(
+	            sessionFactory);
+	 
+	    return transactionManager;
+	}
+}
+	
+	
+	
+	
+	
+	
+	
+
