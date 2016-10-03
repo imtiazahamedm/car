09dc615e20a905b9bf6e0391926339e123fdac1e
imtiazahamedm <imtiazcrazy@gmail.com>
2016-10-03 16:10:29 +0530

    Add files via upload

--- CarMaxxBackEnd/src/main/java/com/CarMaxxBackEndconfig/ApplicationContextConfig.java
+++ CarMaxxBackEnd/src/main/java/com/CarMaxxBackEndconfig/ApplicationContextConfig.java
@@ -5,22 +5,36 @@ import java.util.Properties;
 import javax.sql.DataSource;
 
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
-import org.h2.engine.User;
+
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate3.HibernateTransactionManager;
+import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import com.CarMaxxmodel.User;
 
 @Configuration
 @ComponentScan("com.CarMaxxBackEndconfig")
 @EnableTransactionManagement
 
-
 public class ApplicationContextConfig {
+	
+	@Autowired
+	@Bean(name = "sessionFactory")
+	public SessionFactory getSessionFactory(DataSource dataSource) {
+	 
+	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
+	    sessionBuilder.addProperties(getHibernateProperties());
+	 System.out.println("before");
+	    sessionBuilder.addAnnotatedClasses(User.class);
+	 System.out.println("after");
+	    return sessionBuilder.buildSessionFactory();
+	    
+	}
  
 	@Bean(name = "dataSource")
 	public DataSource getDataSource() {
@@ -33,23 +47,6 @@ public class ApplicationContextConfig {
 	    return dataSource;
 	}
 	@Autowired
-	@Bean(name = "sessionFactory")
-	public SessionFactory getSessionFactory(DataSource dataSource) {
-	 
-	    LocalSessionFactoryBuilder sessionBuilder = new LocalSessionFactoryBuilder(dataSource);
-	 
-	    sessionBuilder.addAnnotatedClasses(User.class);
-	 
-	    return sessionBuilder.buildSessionFactory();
-	    
-	}
-	private Properties getHibernateProperties() {
-	    Properties properties = new Properties();
-	    properties.put("hibernate.show_sql", "true");
-	    properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-	    return properties;
-	}
-	@Autowired
 	@Bean(name = "transactionManager")
 	public HibernateTransactionManager getTransactionManager(
 	        SessionFactory sessionFactory) {
@@ -58,6 +55,15 @@ public class ApplicationContextConfig {
 	 
 	    return transactionManager;
 	}
+	
+	private Properties getHibernateProperties() {
+	    Properties properties = new Properties();
+	    properties.put("hibernate.show_sql", "true");
+	    properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+	    properties.put("hibernate.hbm2ddl.auto","create");
+	    return properties;
+	}
+	
 }
 	
 	
--- CarMaxxBackEnd/src/main/java/com/niit/carmaxxbackend/App.java
+++ CarMaxxBackEnd/src/main/java/com/niit/carmaxxbackend/App.java
@@ -1,5 +1,7 @@
 package com.niit.carmaxxbackend;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        AnnotationConfigApplicationContext ob = new  AnnotationConfigApplicationContext();
+       System.out.println("one");
+        ob.scan("com.CarMaxxBackEndconfig");
+        System.out.println("two");
+        ob.refresh();
+        System.out.println("three");
     }
 }
